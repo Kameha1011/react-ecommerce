f45f1d615628f410bceb77d7a0ce3c64083c0943
Kameha1011 <omarcitofc2001@gmail.com>
2023-03-28 16:02:00 -0300

    channged topsales background

--- src/components/TopSales.jsx
+++ src/components/TopSales.jsx
@@ -1,6 +1,6 @@
 function TopSales() {
   return (
-    <section className='container-fluid d-flex flex-column align-items-center justify-content-center text-light' id='TopSalesContainer'>
+    <section className='container-fluid d-flex text-dark flex-column align-items-center justify-content-center text-light' id='TopSalesContainer'>
         <h2 className='display-1'>WINTER SALES</h2>
         <small>Up to</small>
         <h2 className='discount'>90%</h2>
